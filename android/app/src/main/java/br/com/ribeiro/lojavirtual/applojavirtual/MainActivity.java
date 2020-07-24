a302ed06c844d82f2476faf34bc6d3f511832dfc
andresnsn <59550765+andresnsn@users.noreply.github.com>
2020-07-24 16:39:39 -0300

    AppLojaVirtual

---
+++ android/app/src/main/java/br/com/ribeiro/lojavirtual/applojavirtual/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.ribeiro.lojavirtual.applojavirtual;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:applojavirtual/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter's Clothing",
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
+        
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),   
+    );
+  }
+}
